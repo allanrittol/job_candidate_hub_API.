c791ccff33e61f5f261a1faac5a9f39ddd153d47
Rittol <78919320+Rittol@users.noreply.github.com>
2024-08-11 23:34:42 +0300

    API updates

--- job_candidate_hub_API/Controllers/JobCandidateController.cs
+++ job_candidate_hub_API/Controllers/JobCandidateController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using job_candidate_hub_API.Models;
 using job_candidate_hub_API.Data;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace job_candidate_hub_API.Controllers
 {
@@ -15,6 +16,36 @@ namespace job_candidate_hub_API.Controllers
         {
             _context = context;
         }
+        [HttpPost]
+        public async Task<IActionResult> CreateEdit([FromBody] job_candidate candidate)
+        {
+            if (string.IsNullOrEmpty(candidate.Email))
+            {
+                return BadRequest("Email is required.");
+            }
+                var existingCandidate = await _context.Candidates
+                .FirstOrDefaultAsync(c => c.Email == candidate.Email);
 
+            if (existingCandidate != null)
+            {
+                //update existing candidate
+                existingCandidate.FirstName = candidate.FirstName;
+                existingCandidate.LastName = candidate.LastName;
+                existingCandidate.PhoneNumber = candidate.PhoneNumber;
+                existingCandidate.TimeAvailable = candidate.TimeAvailable;
+                existingCandidate.Email = candidate.Email;
+                existingCandidate.LinkedInURL = candidate.LinkedInURL;
+                existingCandidate.GitHubProfileURL = candidate.GitHubProfileURL;
+                _context.Candidates.Add(existingCandidate);
+            }
+            else
+            {
+                //Create new candidate
+                _context.Candidates.Add(candidate);
+            }
+            await _context.SaveChangesAsync();
+            return Ok();
+            
+        }
     }
 }
--- job_candidate_hub_API/Data/JobCandidatesContext.cs
+++ job_candidate_hub_API/Data/JobCandidatesContext.cs
@@ -1,11 +1,24 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using Microsoft.EntityFrameworkCore;
 using job_candidate_hub_API.Models;
 
 namespace job_candidate_hub_API.Data
 {
     public class JobCandidatesContext : DbContext
     {
-        public JobCandidatesContext(DbContextOptions<JobCandidatesContext> options) : base(options) { }
+        public JobCandidatesContext(DbContextOptions<JobCandidatesContext> options) : base(options)
+        {
+        }
         public DbSet<job_candidate> Candidates { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var entity = modelBuilder.Entity<job_candidate>();
+            modelBuilder.Entity<JobCandidatesContext>();
+            {
+                entity.HasKey(e => e.Email);
+            }; 
+        }
+
+ 
     }
 }
